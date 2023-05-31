f6689299279b1d7e767a59b7d3c4bfda75c8f456
chris-kewmann <christson.hartono@kewmann.com>
2023-05-31 13:44:41 +0700

    refactor: change config class

--- app/config/config.py
+++ app/config/config.py
@@ -2,40 +2,50 @@ import os
 import logging
 import mlflow
 from dotenv import load_dotenv
+from functools import lru_cache
 from func_timeout import func_set_timeout
+from pydantic import BaseSettings
 load_dotenv()
-
-# Set all configuration variables here
-# Database Variables
-DB_POSTRES_USERNAME = os.getenv('DB_POSTGRES_USERNAME')
-DB_POSTRES_PASSWORD = os.getenv('DB_POSTGRES_PASSWORD')
-DB_POSTGRES_HOST = os.getenv('DB_POSTGRES_HOST')
-DB_POSTGRES_PORT = os.getenv('DB_POSTGRES_PORT')
-DB_POSTGRES_NAME = os.getenv('DB_POSTGRES_NAME')
-DB_POSTGRES_URI = f'postgresql://{DB_POSTRES_USERNAME}:{DB_POSTRES_PASSWORD}@{DB_POSTGRES_HOST}:{DB_POSTGRES_PORT}/{DB_POSTGRES_NAME}'
-
-# DB_MYSELF_USERNAME =
-# DB_MYSQL_PASSWORD =
-# DB_MYSQL_HOST =
-# DB_MYSEL_NAME =
-
-# Elastic Search Variables
-# ES_HOST =
-# ES_PORT =
-
-# Mlflow Variables
-MLFLOW_HOST = os.getenv('MLFLOW_HOST')
-MLFLOW_PORT = os.getenv('MLFLOW_PORT')
-MLFLOW_TRACKING_URI = f'http://{MLFLOW_HOST}:{MLFLOW_PORT}'
-MLFLOW_EXPERIMENT_NAME = os.getenv('MLFLOW_EXPERIMENT_NAME')
-
-# Security Variables
-ENCRYPT_KEY = os.getenv('ENCRYPT_KEY')
 
 # Non-variable configuration
 logger = logging.getLogger(__name__)
 
-class Config:
+# Base Settings Class
+class Settings(BaseSettings):
+    # Postgres database variables
+    db_postgres_username: str = 'postgres'
+    db_postgres_password: str = 'postgres'
+    db_postgres_host: str = '127.0.0.1'
+    db_postgres_port: int = 5432
+    db_postgres_name: str = 'kewdetect'
+
+    # Mlflow variables
+    mlflow_host: str = '127.0.0.1'
+    mlflow_port: int = 5000
+    mlflow_experiment_name: str = 'kewdetectkewdetect_models'
+
+    # Security variables
+    encrypt_key: str
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+# Get base settings function
+@lru_cache()
+def get_settings():
+    return Settings()
+
+# Base setting object
+settings = get_settings()
+
+# Database Variables
+DB_POSTGRES_URI = f'postgresql://{settings.db_postgres_username}:{settings.db_postgres_password}@{settings.db_postgres_host}:{settings.db_postgres_port}/{settings.db_postgres_name}'
+
+# Mlflow Variables
+MLFLOW_TRACKING_URI = f'http://{settings.mlflow_host}:{settings.mlflow_port}'
+
+class MlflowSetup:
     """
     This configuration is only set once and will be used several times during the service lifetime
     components:
@@ -56,10 +66,9 @@ class Config:
         try:
             mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-            mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
+            mlflow.set_experiment(settings.mlflow_experiment_name)
 
             self.mlflow_client = mlflow.MlflowClient()
         except Exception:
             logger.critical("failed to connect mlflow")
             raise
-
--- app/core/utils.py
+++ app/core/utils.py
@@ -5,15 +5,6 @@ from app.core.connection.postgres import engine
 from app.database.v1.models.rule import Base as BaseRule
 from app.database.v1.models.transaction import Base as BaseTransaction
 
-
-"""
-def fetch_mlflow_data(run_id:str):
-    data = app.conf.mlflow_client.get_run(run_id).data
-    tags = {k: v for k, v in data.tags.items() if not k.startswith("mlflow.")}
-    artifacts = [f.path for f in app.conf.mlflow_client.list_artifacts(run_id, "model")]
-    return data.params, data.metrics, tags, artifacts
-"""
-
 def create_migration():
     # Create table if not exist
     BaseRule.metadata.drop_all(bind=engine)
@@ -26,7 +17,7 @@ def encrypt(inp: str):
     try:
         if not isinstance(inp, bytes):
             inp = inp.encode(encoding='utf-8')
-        f = Fernet(config.ENCRYPT_KEY)
+        f = Fernet(config.settings.encrypt_key)
         result = f.encrypt(inp).decode()
     except InvalidToken:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY , detail=f'invalid id')
@@ -35,7 +26,7 @@ def encrypt(inp: str):
 
 def decrypt(inp: str):
     try:
-        f = Fernet(config.ENCRYPT_KEY)
+        f = Fernet(config.settings.encrypt_key)
         result = f.decrypt(inp).decode(encoding='utf-8')
     except InvalidToken:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY , detail=f'invalid id')
